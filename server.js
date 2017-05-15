a614bee45cdd4012278b8f0e81f94e701c45a266
Kaylee Alvarado <kaymay1000@gmail.com>
2017-05-15 15:12:46 -0700

    first user get route tests passing

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
 'use strict'
 
-const express = require('express')
+const express = require('express');
 
-const mongoose = require('mongoose')
-const morgan = require('morgan')
-const authRouter = require('./route/auth-router.js')
-const devRouter = require('./route/dev-router')
-const npoRouter = require('./route/npo-router.js')
-const errorMiddleware = require('./lib/error-midd.js')
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const authRouter = require('./route/auth-router.js');
+const devRouter = require('./route/dev-router');
+const npoRouter = require('./route/npo-router.js');
+const errorMiddleware = require('./lib/error-midd.js');
 
-const app = express()
+const app = express();
 
 //local mongo db will be called 'devolunteer'
-const PORT = process.env.PORT || 3000
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/devolunteer'
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/devolunteer';
 
-mongoose.connect(MONGODB_URI)
-mongoose.Promise = Promise
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
 
-app.use(morgan('dev'))
-app.use(authRouter)
-app.use(devRouter)
-app.use(npoRouter)
-app.use(errorMiddleware)
+app.use(morgan('dev'));
+app.use(authRouter);
+app.use(devRouter);
+app.use(npoRouter);
+app.use(errorMiddleware);
 
-module.exports = app
+module.exports = app;
 
 if(require.main === module) {
   app.listen(PORT, () => {
