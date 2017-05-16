313e665c51d54fe4e9215230ef3d19996a470d2d
Carlo Santos <carlox7@gmail.com>
2017-05-16 12:30:24 -0700

    finished npo routes and controllers

--- controller/npo-controller.js
+++ controller/npo-controller.js
@@ -8,20 +8,20 @@ exports.createNpo = function(body, user) {
 
   body.userID = user._id;
   return new Npo(body).save()
-  .then(dev => dev)
+  .then(npo => npo)
   .catch(err => body.status(err.status).send(err.message));
 };
 
 exports.fetchNpo = function(id, res) {
 
   return Npo.findById(id)
-  .then( dev => dev)
+  .then( npo => npo)
   .catch(err => res.status(err.status).send(err.message));
 };
 
 exports.updateNpo = function(req, res, id) {
   return Npo.findByIdAndUpdate(id, req.body, {new:true})
-  .then(dev => dev)
+  .then(npo => npo)
   .catch(err => res.status(err.status).send(err.message));
 };
 
