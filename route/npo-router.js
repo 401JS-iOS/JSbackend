cf77e5eca8ccfd1aaebc4cd5fffbb66a204878ac
Chris Closser <chrisclosser1@gmail.com>
2017-05-16 12:14:46 -0700

    using coveralls lint and travis

--- route/npo-router.js
+++ route/npo-router.js
@@ -1,39 +1,39 @@
 //create a post to create profile for an NPO
-let Router = require('express').Router
-let bearerAuth = require('../lib/bearer-auth-midd.js')
-let createError = require('http-errors')
-let Npo = require('../model/npo')
-let jsonParser = require('body-parser').json()
+let Router = require('express').Router;
+let bearerAuth = require('../lib/bearer-auth-midd.js');
+let createError = require('http-errors');
+let Npo = require('../model/npo');
+let jsonParser = require('body-parser').json();
 
-let router = module.exports = new Router()
+let router = module.exports = new Router();
 
 
 //req.user should be a individual user which the bearer auth will identify
 router.post('/api/npo', bearerAuth, jsonParser, (req, res, next) => {
-  if(!req.user.isNPO) return next(createError(401, 'Please log in as a Non Profit Organization'))
+  if(!req.user.isNPO) return next(createError(401, 'Please log in as a Non Profit Organization'));
 
   //req.body will be values from the form they fill out on angular front-end
-  const npo = new Npo(req.body)
+  const npo = new Npo(req.body);
   npo.save()
   .then(npo => res.json(npo))
-  .catch(next)
-})
+  .catch(next);
+});
 
 router.get('/api/npo', bearerAuth, (req, res, next) => {
-  if(!req.user.isNPO) return next(createError(401, 'please log in as an NPO'))
+  if(!req.user.isNPO) return next(createError(401, 'please log in as an NPO'));
 
   Npo.find()
   .then( npo => {
-    return res.json(npo)
+    return res.json(npo);
   })
-  .catch(next)
-})
+  .catch(next);
+});
 
 router.delete('/api/npo', bearerAuth, (req, res) => {
   Npo.findByIdAndRemove(req.user.id)
   .then(npo => res.json(npo))
   .catch(e => {
-    console.log(e)
-    res.json({}) //or err.message?
-  })
-})
+    console.log(e);
+    res.json({}); //or err.message?
+  });
+});
